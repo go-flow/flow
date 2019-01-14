c53577eb61e21c602a666a3da388481fd0aaf747
Sedin Dugum <sedin.dugum@gmail.com>
2019-01-14 15:48:45 +0100

    updated VM comment

--- vm.go
+++ vm.go
@@ -2,5 +2,5 @@ package flow
 
 // VM stands for  ViewModel which is map[string]interface{}
 //
-// it is tipically used for passing data to render.Renderer
+// it is tipically used for passing data from action (Controller) to render.Renderer (view)
 type VM map[string]interface{}
